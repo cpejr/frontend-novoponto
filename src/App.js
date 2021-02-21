e9d66a282c64f971100a79a9fac83422bb89c247
Diogo Almazan <diogodpa@outlook.com>
2021-02-21 19:33:25 -0300

    GlobalStyle Styled Components

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import Routes from './routes';
 import ThemeContextProvider from './context/ThemeProvider';
 
+import GlobalStyle from './styles/GlobalStyle';
+
 function App() {
   return (
     <ThemeContextProvider>
+      <GlobalStyle />
       <Routes />
     </ThemeContextProvider>
   );
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+    *{
+        box-sizing: border-box;
+    }
+
+
+    body {
+        margin: 0;
+        padding: 0;
+        
+        font-family: 'Inter', BlinkMacSystemFont, 'Segoe UI', 'Roboto', sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+
+    h1, h2, h3, h4{
+        margin: 0;
+    }
+`;
+
+export default GlobalStyle;
