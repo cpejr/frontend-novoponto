0bbb3294c454ef93986af83513cffa807705873b
ArthurLima <arthurdeolima@gmail.com>
2021-04-13 21:34:13 -0300

    Pagina de acompanhamento linkada com back

--- src/pages/Administration/HourFollowing/index.js
+++ src/pages/Administration/HourFollowing/index.js
@@ -1,78 +1,86 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import { gql, useQuery, useMutation } from "@apollo/client";
 import { SelectInput } from "../../../components/atoms";
 import MemberDataADM from "../../../components/organisms/MemberDataADM";
-import { message } from 'antd';
+import { message } from "antd";
 
-import {HourFollowingContainer} from './styles'
+import { HourFollowingContainer } from "./styles";
 
-const membersConst = [
-	{
-		id: 1,
-		name: "Arthur Lima",
-		message: "Ponts de acompanhamento: 2 pontos",
-		responsable: {
-			name: "Maiara Lima"
-		},
-		role: {
-			name: "Head de Projetos"
-		}
-	}, 
-	{
-		id: 2,
-		name: "Arthur Braga",
-		message: "Ponts de acompanhamento: 1 pontos",
-		responsable: {
-			name: "Ian Xavier"
-		},
-		role: {
-			name: "Head de Marketing"
-		}
-	}
-];
+const GET_MEMBERS = gql`
+  query GetMembers {
+    members {
+      name
+      _id
+      message
+      responsible {
+        name
+      }
+      role {
+        name
+      }
+    }
+  }
+`;
+
+const UPDATE_MEMBER = gql`
+  mutation UPDATE_MEMBER($memberId: ID!, $data: MemberUpdate) {
+    updateMember(memberId: $memberId, data: $data) {
+      message
+    }
+  }
+`;
 
 const HourFollowing = () => {
-  const [members, setMembers] = useState([]);
   const [selected, setSelected] = useState();
 
   const selectMember = (memberSelected) => {
-    var index = members
-      .map((member) => member.name)
-      .indexOf(memberSelected);
+    var index = members.map((member) => member.name).indexOf(memberSelected);
     setSelected(members[index]);
   };
 
-  const saveData = (newMessage) => {
-	  var index = members
-      .map((member) => member.id)
-      .indexOf(selected.id);
-	  members[index].message = newMessage;
-	  setMembers([...members]);
-	message.loading("Salvando", 1.5).then(()=>{
-		message.success("Salvo com sucesso", 2.5);
-	})
+  const saveData = async (newMessage) => {
+    var hide = message.loading("Salvando");
+		try{
+			await updateMember({
+				variables: { memberId: selected._id, data: { message: newMessage } },
+			});
+			hide();
+			message.success("Salvo com sucesso", 2.5);
+			refetch();
+		}
+		catch (err) {
+			console.log(err);
+			message.error("Houve um problema, tente novamente", 2.5);
+		}
   };
 
-  useEffect(() => {
-    setMembers([...membersConst]);
-  }, []);
-  return (
-    <HourFollowingContainer>
-      <SelectInput
-        options={members.map((member) => member.name)}
-        callback={selectMember}
-      />
-			{
-				selected && 
-				<MemberDataADM
-					name={selected.name}
-					responsable={selected.responsable.name}
-					role={selected.role.name}
-					initMessage={selected.message}
-					callback={saveData}
-				/>
-			}
-    </HourFollowingContainer>
-  );
+  const [updateMember] = useMutation(UPDATE_MEMBER);
+  const { loading, error, data, refetch } = useQuery(GET_MEMBERS);
+
+  if (loading) return "Loading...";
+  else if (error) return `Error! ${error.message}`;
+  else if (data) {
+    var members = data.members;
+    return (
+      <HourFollowingContainer>
+        <SelectInput
+          options={members.map((member) => member.name)}
+          callback={selectMember}
+        />
+        {selected && (
+          <MemberDataADM
+            name={selected.name}
+            responsable={
+              selected.responsible ? selected.responsible.name : "Indefinido"
+            }
+            role={selected.role ? selected.role.name : "Indefinido"}
+            initMessage={selected.message}
+            callback={saveData}
+          />
+        )}
+      </HourFollowingContainer>
+    );
+  }
 };
 
-export default HourFollowing
+export default HourFollowing;
