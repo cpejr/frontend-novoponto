fc66c8ed80e65aed7a26d8d36d43e04e41f74a0d
Arthur Martins Braga <arthurmbraga1@gmail.com>
2021-04-21 02:36:30 -0300

    Feat: tabela horarios adicionais

--- src/components/molecules/HomeOfficeTable/index.js
+++ src/components/molecules/HomeOfficeTable/index.js
@@ -1,92 +1,26 @@
 import React, { useContext, useState, useEffect } from "react";
 import { ThemeContext } from "../../../context/ThemeProvider";
 import { SessionContext } from "../../../context/SessionProvider";
-import { HourDisplayer, InfoDisplayer, DefaultText } from "../../atoms";
-import moment from "moment";
 
-import { AditionalHourTableArea, FlexDiv } from "./styles";
-import { Popover, Table, Collapse } from "antd";
-import { InfoCircleOutlined } from "@ant-design/icons";
+import { AditionalHourTableArea } from "./styles";
+import { Table, Collapse } from "antd";
+import { getColumns } from "./columns";
 
 const HomeOfficeTable = ({ aditionalHours }) => {
   const { themeColors } = useContext(ThemeContext);
   const { data } = useContext(SessionContext);
 
-  const [commentVisible, setCommentVisible] = useState(false);
+  const [columns, setColums] = useState();
 
   useEffect(() => {
     const visible =
       data?.member?.role?.access > 0 ||
       !!aditionalHours?.find((item) => !!item.description);
-    setCommentVisible(visible);
-  }, [aditionalHours]);
 
-  function getOperation(op) {
-    switch (op) {
-      case "ADD":
-        return { text: "Adicionar", color: "green" };
-      case "REMOVE":
-        return { text: "Remover", color: "red" };
-      default:
-        return { text: "Erro...", color: "yellow" };
-    }
-  }
+    setColums(getColumns(themeColors, visible));
 
-  const columns = [
-    {
-      title: "Dia",
-      dataIndex: "date",
-      key: "date",
-      render: (text) => (
-        <DefaultText style={{ margin: 0 }}>
-          {" "}
-          {moment(text).format("DD/MM/yy")}
-        </DefaultText>
-      ),
-    },
-    {
-      title: "Tipo",
-      dataIndex: "action",
-      key: "action",
-      render: (action) => (
-        <FlexDiv>
-          <InfoDisplayer
-            info={getOperation(action).text}
-            infoColor={themeColors[getOperation(action).color]}
-          />
-        </FlexDiv>
-      ),
-    },
-    {
-      title: "Tempo",
-      dataIndex: "formatedAmount",
-      key: "formatedAmount",
-      render: (formatedAmount) => (
-        <FlexDiv>
-          <HourDisplayer
-            dateOrTime={"time"}
-            hour={formatedAmount}
-            hourColor={themeColors.yellow}
-          />
-        </FlexDiv>
-      ),
-    },
-    {
-      title: "O que fez",
-      dataIndex: "description",
-      key: "description",
-      render: (description) => (
-        <a>
-          {JSON.stringify(description)}
-          <Popover content={description} title="O que eu fiz">
-            <InfoCircleOutlined
-              style={{ fontSize: "2em", color: themeColors.yellow }}
-            />
-          </Popover>
-        </a>
-      ),
-    },
-  ];
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [aditionalHours, themeColors]);
 
   return (
     <AditionalHourTableArea>
--- src/components/molecules/HomeOfficeTable/styles.js
+++ src/components/molecules/HomeOfficeTable/styles.js
@@ -2,61 +2,6 @@ import styled from "styled-components";
 
 const AditionalHourTableArea = styled.div`
   width: 100%;
-  margin-bottom: 24px;
-
-  h3 {
-    margin-bottom: 16px;
-    color: #ffffff;
-  }
-
-  .justificationTable {
-    width: 100%;
-  }
-  .justificationTable tr {
-    height: 80px;
-    background-color: #141414;
-    padding: 0px 20px;
-
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    border-bottom: 2px solid #1c1c1d;
-  }
-
-  .justificationTable thead tr:first-child {
-    height: 65px;
-    background-color: #1d1d1d;
-
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    border-radius: 5px;
-  }
-
-  .dayColumn {
-    min-width: 120px;
-    display: flex;
-    justify-content: flex-start;
-    color: #ffffff;
-  }
-
-  .timeArea,
-  .typeArea {
-    width: 30%;
-    min-width: 120px;
-    display: flex;
-    justify-content: flex-start;
-    color: #ffffff;
-  }
-
-  .justificationTablesArea {
-    width: 100%;
-    margin-bottom: 48px;
-  }
-
-  .descriptionArea {
-    align-items: center;
-  }
 `;
 
 const FlexDiv = styled.div`
