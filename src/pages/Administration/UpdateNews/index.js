54717628fbcf1da7859aeba825541d736a683f5c
Diogo Almazan <diogodpa@outlook.com>
2021-06-12 01:22:41 -0300

    Página Atualizar Notícias em andamento

--- src/pages/Administration/UpdateNews/index.js
+++ src/pages/Administration/UpdateNews/index.js
@@ -1,47 +1,68 @@
-import React, { useContext } from 'react';
-import { UpdatedNewsComponent } from './styles';
-import { ThemeContext } from '../../../context/ThemeProvider';
+import React, { useContext } from "react";
+import { UpdatedNewsComponent, SingleNewsBox } from "./styles";
+import { ThemeContext } from "../../../context/ThemeProvider";
 
-import Lottie from "react-lottie";
-import buildingLottie from "../../../assets/lotties/building.json";
-import { DefaultText } from "../../../components/atoms";
+import assignmentIcon from "../../../assets/assignmentIcon.svg";
+import addCircleIcon from "../../../assets/addCircleIcon.svg";
+import deleteIcon from "../../../assets/deleteIcon.svg";
+import { useState } from "react";
+
+const NewsBox = ({ howManyNews, handleDelete }) => {
+  return (
+    <SingleNewsBox>
+      <div className="titleAreaSingleNewsBox">
+        <span>{`Notícia ${howManyNews}`}</span>
+        <img
+          src={deleteIcon}
+          alt="Apagar notícia"
+          onClick={() => handleDelete(howManyNews)}
+        />
+      </div>
+
+      <div>Summernote</div>
+    </SingleNewsBox>
+  );
+};
 
 const UpdatedNews = () => {
-    const { themeColors } = useContext(ThemeContext);
-    const defaultOptions = {
-        loop: true,
-        autoplay: true,
-        rendererSettings: {
-          preserveAspectRatio: "xMidYMid slice",
-        },
-      };
+  const { themeColors } = useContext(ThemeContext);
 
-    return (
-        <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        height: "100vh",
-        justifyContent: "center",
-        maxWidth: 600,
-        textAlign: "center",
-        margin: "auto",
-      }}
-    >
-      <Lottie
-        options={{ ...defaultOptions, animationData: buildingLottie }}
-        height={300}
-        width={300}
-      />
-      <DefaultText style={{ fontSize: 30, marginBottom: 8 }}>
-        Em construção
-      </DefaultText>
-      <DefaultText style={{ opacity: 0.5 }}>
-        (isso pode levar um tempo)
-      </DefaultText>
-    </div>
-    );
-}
+  const [numberOfNewsBox, setNumberOfNewsBox] = useState([1]);
+
+  const handleAddNewsBox = () => {
+    setNumberOfNewsBox([...numberOfNewsBox, 1]);
+  };
+
+  const handleDeleteNewsBox = (whichNewsBoxIndex) => {
+    numberOfNewsBox.splice(whichNewsBoxIndex, 1);
+    setNumberOfNewsBox([...numberOfNewsBox]);
+  };
+
+  return (
+    <UpdatedNewsComponent>
+      <div className="titleArea">
+        <img src={assignmentIcon} alt="Atualizar Notícias" />
+        <h1>Atualizar notícias</h1>
+      </div>
+
+      <div className="outerBoxNewsContainer">
+        <span>Bloco de notícia</span>
+        <div className="innerBoxNewsContainer">
+          <img
+            src={addCircleIcon}
+            alt="Adicionar notícia"
+            onClick={() => handleAddNewsBox()}
+          />
+          {numberOfNewsBox.map((_, index) => (
+            <NewsBox
+              howManyNews={index + 1}
+              handleDelete={handleDeleteNewsBox}
+            />
+          ))}
+        </div>
+      </div>
+    </UpdatedNewsComponent>
+  );
+};
 
 export default UpdatedNews;
--- src/pages/Administration/UpdateNews/styles.js
+++ src/pages/Administration/UpdateNews/styles.js
@@ -1,10 +1,92 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const UpdatedNewsComponent = styled.div`
-    width: 100%;
-    min-height: 100vh;
+  width: 100%;
+  min-height: 100vh;
 
-    background-color: ${props => props.theme.appBackground}
+  padding: 30px 20px;
+
+  background-color: ${(props) => props.theme.appBackground};
+
+  display: flex;
+  flex-direction: column;
+
+  .titleArea {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+  }
+
+  .titleArea img {
+    margin-right: 16px;
+  }
+
+  .titleArea h1 {
+    font-size: 24px;
+    font-weight: 400;
+  }
+
+  .outerBoxNewsContainer {
+    max-width: 1200px;
+    margin: 0 auto;
+    margin-top: 40px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+  .outerBoxNewsContainer > span {
+    margin-bottom: 16px;
+    font-size: 14px;
+    font-weight: 700;
+  }
+
+  .innerBoxNewsContainer {
+    width: 100%;
+    position: relative;
+
+    display: grid;
+    grid-template-columns: repeat(2, minmax(250px, 1fr));
+    justify-items: center;
+    row-gap: 60px;
+    column-gap: 80px;
+
+    padding: 70px 80px;
+
+    border: 2px solid #454545;
+    border-radius: 5px;
+  }
+
+  .innerBoxNewsContainer > img {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    cursor: pointer;
+  }
 `;
 
-export  { UpdatedNewsComponent };
+const SingleNewsBox = styled.div`
+  width: 100%;
+  max-width: 500px;
+  min-height: 100px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  .titleAreaSingleNewsBox {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    margin-bottom: 8px;
+  }
+
+  .titleAreaSingleNewsBox img {
+    cursor: pointer;
+  }
+`;
+
+export { UpdatedNewsComponent, SingleNewsBox };
