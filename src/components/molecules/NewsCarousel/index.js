f8eb2fd56c91d51f8799bed1e2a3273c7e66ccab
Arthur Martins Braga <arthurmbraga1@gmail.com>
2021-06-17 20:16:30 -0300

    Feat: News

---
+++ src/components/molecules/NewsCarousel/index.js
@@ -0,0 +1,53 @@
+import React, { useContext } from "react";
+import { Carousel } from "react-responsive-carousel";
+import Ratio from "react-ratio/lib/Ratio";
+
+import { NewsCarouselContainer } from "./styles";
+import defaultNews from "../../../assets/defaultNews.svg";
+import { NewsItem } from "../../atoms";
+import { GlobalsContext } from "../../../context/GlobalsProvider";
+
+const news = [
+  <img src={defaultNews} className="defaultNews" alt="Confira as novidades" />,
+  <img src={defaultNews} className="defaultNews" alt="Confira as novidades" />,
+  <img src={defaultNews} className="defaultNews" alt="Confira as novidades" />,
+  <img src={defaultNews} className="defaultNews" alt="Confira as novidades" />,
+];
+
+const NewsCarousel = ({ ...props }) => {
+  const { width } = useContext(GlobalsContext);
+
+  const newsPerSlide = width > 1400 ? 2 : 1;
+
+  const newsWrapper = [];
+
+  for (let i = 0; i < news.length; i += newsPerSlide) {
+    const newsGroup = [];
+
+    for (let j = 0; j < newsPerSlide; j++) {
+      const element = news[j + i];
+
+      if (element) newsGroup[j] = <NewsItem>{element}</NewsItem>;
+    }
+
+    newsWrapper.push(
+      <Ratio
+        ratio={(16 * newsPerSlide) / 9}
+        className="newsRadio"
+        contentClassName="newsRadio"
+      >
+        <div className="newsRow">{newsGroup}</div>
+      </Ratio>
+    );
+  }
+
+  return (
+    <NewsCarouselContainer {...props}>
+      <Carousel infiniteLoop showStatus={false}>
+        {newsWrapper}
+      </Carousel>
+    </NewsCarouselContainer>
+  );
+};
+
+export default NewsCarousel;
---
+++ src/components/molecules/NewsCarousel/styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const NewsCarouselContainer = styled.div`
+  .newsRadio {
+    max-height: 342px;
+  }
+
+  .newsRow {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    justify-content: space-around;
+  }
+
+  .defaultNews {
+    max-width: 226px;
+    max-height: 171px;
+  }
+`;
+
+export { NewsCarouselContainer };
