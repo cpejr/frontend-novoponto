6c7ff37638fde8b048b2484b441ef39a8b3f0602
Arthur Martins Braga <arthurmbraga1@gmail.com>
2021-06-29 16:25:26 -0300

    Fix: bugs

--- src/pages/Profile/login.js
+++ src/pages/Profile/login.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { GoogleOutlined } from "@ant-design/icons";
 import { CommonButton, DefaultText } from "../../components/atoms";
 import { GoogleLogin } from "react-google-login";
@@ -6,6 +6,7 @@ import { Skeleton } from "antd";
 import { SessionContext } from "../../context/SessionProvider";
 
 const CLIENT_ID =
+  process.env.REACT_APP_CLIENT_ID ||
   "927681508740-6avqgv44im25umcj7ji2856o84fcrhje.apps.googleusercontent.com";
 
 const Login = () => {
