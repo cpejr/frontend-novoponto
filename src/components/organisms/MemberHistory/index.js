ca79eab31399943238b1f9dd1d331040d4306dc2
João Pedro Lima Pirajá <jplp100@hotmail.com>
2022-10-02 21:25:53 -0300

    fix: now, when consulting a member's hours, the last day of the range counts for the check.

--- src/components/organisms/MemberHistory/index.js
+++ src/components/organisms/MemberHistory/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { DatePicker, Space } from "antd";
+import { DatePicker } from "antd";
 import moment from "moment";
 import { useLazyQuery } from "@apollo/client";
 
@@ -9,66 +9,65 @@ import SessionsTable from "../../molecules/SessionsTable";
 import { MemberHistoyContainer } from "./styles";
 const { RangePicker } = DatePicker;
 
-const MemberHistory = ({ memberId, ...props }) => {
-  const [rangeDate, setRangeDate] = useState([
-    moment().startOf("isoWeek"),
-    moment(),
-  ]);
+const MemberHistory = ({ memberId }) => {
+	const [rangeDate, setRangeDate] = useState([
+		moment().startOf("isoWeek"),
+		moment().endOf("day"),
+	]);
 
-  const startDate = rangeDate && rangeDate[0];
-  const endDate = rangeDate && rangeDate[1];
+	const startDate = rangeDate && rangeDate[0];
+	const endDate = rangeDate && rangeDate[1];
 
-  const [loadCompiled, { loading, data }] = useLazyQuery(FetchCompiledForHC, {
-    fetchPolicy: "network-only",
-  });
+	const [loadCompiled, { loading, data }] = useLazyQuery(FetchCompiledForHC, {
+		fetchPolicy: "network-only",
+	});
 
-  const { aditionalHours, sessions, formatedTotal } = data?.compiled || {};
+	const { aditionalHours, sessions, formatedTotal } = data?.compiled || {};
+	async function loadData() {
+		return loadCompiled({
+			variables: {
+				memberId,
+				startDate: moment(startDate)?.startOf("day").toISOString(),
+				endDate: moment(endDate)?.endOf("day").toISOString(),
+			},
+		});
+	}
 
-  async function loadData() {
-    return loadCompiled({
-      variables: {
-        memberId,
-        startDate: startDate?.toISOString(),
-        endDate: endDate?.toISOString(),
-      },
-    });
-  }
+	useEffect(() => {
+		if (startDate && endDate && memberId) loadData();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [memberId, rangeDate]);
 
-  useEffect(() => {
-    if (startDate && endDate && memberId) loadData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [memberId, rangeDate]);
+	function disabledDate(current) {
+		// Can not select days after today
+		return current && current > moment().endOf("day");
+	}
 
-  function disabledDate(current) {
-    // Can not select days after today
-    return current && current > moment().endOf("day");
-  }
+	if (memberId && !loading)
+		return (
+			<MemberHistoyContainer>
+				<h5>Histórico Ponto</h5>
 
-  if (memberId && !loading)
-    return (
-      <MemberHistoyContainer>
-        <h5>Histórico Ponto</h5>
+				<RangePicker
+					format="DD-MM-yyyy"
+					disabledDate={disabledDate}
+					onChange={setRangeDate}
+					value={rangeDate}
+					placeholder={["Inicio", "Fim"]}
+				/>
 
-        <RangePicker
-          format="DD-MM-yyyy"
-          disabledDate={disabledDate}
-          onChange={setRangeDate}
-          value={rangeDate}
-          placeholder={["Inicio", "Fim"]}
-        />
-
-        {startDate && endDate && (
-          <div className="mt-4">
-            <SessionsTable sessions={sessions} formatedTotal={formatedTotal} />
-            <HomeOfficeTable
-              aditionalHours={aditionalHours}
-              onDelete={loadData}
-            />
-          </div>
-        )}
-      </MemberHistoyContainer>
-    );
-  else return <></>;
+				{startDate && endDate && (
+					<div className="mt-4">
+						<SessionsTable sessions={sessions} formatedTotal={formatedTotal} />
+						<HomeOfficeTable
+							aditionalHours={aditionalHours}
+							onDelete={loadData}
+						/>
+					</div>
+				)}
+			</MemberHistoyContainer>
+		);
+	else return <></>;
 };
 
 export default MemberHistory;
