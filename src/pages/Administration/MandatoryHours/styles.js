bb589c265734c3f7004e5dca1a4b8499d91dd3a2
Amanda Fernandes <amandaalves@cpejr.com.br>
2023-01-16 11:58:59 -0300

    Horário obrigatório ocultado no menu de adminitração e página do usuário

--- src/pages/Administration/MandatoryHours/styles.js
+++ src/pages/Administration/MandatoryHours/styles.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 const MandatoryHoursComponent = styled.div`
   width: 100%;
   min-height: 100vh;
-
   background-color: ${(props) => props.theme.appBackground};
 `;
 
