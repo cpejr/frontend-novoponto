4e065595f8480521c2eca97057fb405356b11840
gustavolaradossantos <gustavolaradossantos@gmail.com>
2023-02-27 15:19:23 -0300

    linking frontend and backend

--- src/graphql/Tasks.js
+++ src/graphql/Tasks.js
@@ -1,40 +1,39 @@
 import gql from "graphql-tag";
 
 const CREATE_TASK = gql`
-    mutation CreateTask($data: TaskInput!) {
-        createTask(data: $data) {
-            _id
-            name
-            active
-        }
+  mutation CreateTask($data: TaskInput!) {
+    createTask(data: $data) {
+      _id
+      name
+      active
     }
+  }
 `;
 
 const DELETE_TASK = gql`
-    mutation DeleteTask($tribeId: ID!) {
-        deleteTask(taskId: $taskId) {
-            _id
-        }
+  mutation DeleteTask($tribeId: ID!) {
+    deleteTask(taskId: $taskId) {
+      _id
     }
+  }
 `;
-    
+
 const UPDATE_TASK = gql`
-    mutation UpdateTask($taskId: ID!, $data: TaskUpdate) {
-        updateTask(taskId: $taskId, data: $data) {
-            _id
-            name
-        }
+  mutation UpdateTask($taskId: ID!, $data: TaskUpdate) {
+    updateTask(taskId: $taskId, data: $data) {
+      _id
+      name
     }
+  }
 `;
 
-const GET_TASK = gql`
-    query GetTasks {
-        tasks {
-            _id
-            name
-            active
-        }
+const GET_TASKS = gql`
+  query GetTasks {
+    tasks {
+      _id
+      name
     }
+  }
 `;
 
-export { GET_TASK, DELETE_TASK, UPDATE_TASK, CREATE_TASK };
+export { GET_TASKS, DELETE_TASK, UPDATE_TASK, CREATE_TASK };
--- src/pages/Administration/Tasks/index.js
+++ src/pages/Administration/Tasks/index.js
@@ -1,20 +1,10 @@
 import React, { useContext, useEffect, useState } from "react";
-import { GlobalsContext } from "../../../context/GlobalsProvider";
 import { useQuery, useMutation } from "@apollo/client";
-import {
-	UpdateMember,
-	DeleteMember,
-	CreateMember,
-} from "../../../graphql/Member";
 import { Tooltip, message, Skeleton, Table } from "antd";
 import { TasksComponent, ActionsDiv } from "./styles";
 import { ThemeContext } from "../../../context/ThemeProvider";
 
-import {
-	CommonButton,
-	DefaultLabel,
-	InputText,
-} from "../../../components/atoms";
+import { CommonButton, InputText } from "../../../components/atoms";
 import searchIcon from "../../../assets/searchIcon.svg";
 import ConfirmationModal from "../../../components/molecules/ConfirmationModal";
 import FormModal from "../../../components/organisms/FormModal";
@@ -22,260 +12,224 @@ import FormModal from "../../../components/organisms/FormModal";
 import { EditOutlined, RestOutlined, TeamOutlined } from "@ant-design/icons";
 
 import validators from "../../../services/validators";
-import { GET_TRIBES } from "../../../graphql/Tribes";
+import {
+  CREATE_TASK,
+  UPDATE_TASK,
+  DELETE_TASK,
+  GET_TASKS,
+} from "../../../graphql/Tasks";
 import diacriticCaseInsensitiveMatch from "../../../utils/diacriticCaseInsensitiveMatch";
 
-const { Column, ColumnGroup } = Table;
+const { Column } = Table;
 
 const Tasks = () => {
-	const { themeColors } = useContext(ThemeContext);
-	const { tasksLoading, tasksError, allTasksData, refetchTasks } =
-		useContext(GlobalsContext);
+  const { themeColors } = useContext(ThemeContext);
 
-	// const { data: roles, error: errorRoles } = useQuery(GET_ROLES);
-	// const { data: tribes, error: errorTribes } = useQuery(GET_TRIBES);
+  const [updateTaskMutation] = useMutation(UPDATE_TASK);
+  const [createTaskMutation] = useMutation(CREATE_TASK);
+  const [deleteTaskMutation] = useMutation(DELETE_TASK);
+  const { loading, error, data, refetch } = useQuery(GET_TASKS);
+  console.log(data);
 
-	const [updateTaskMutation] = useMutation(UpdateMember);
-	const [createTaskMutation] = useMutation(CreateMember);
-	const [deleteTaskMutation] = useMutation(DeleteMember);
-	const { loading, error, data, refetch } = useQuery(GET_TRIBES);
+  const [openModalExcludeTask, setOpenModalExcludeTask] = useState(false);
+  const [filteredTasks, setFilteredTasks] = useState([]);
+  const [excludeTask, setExcludeTask] = useState({});
+  const [editOrCreateModalInfo, setEditOrCreateModalInfo] = useState({
+    open: false,
+  });
 
-	const [openModalExcludeTask, setOpenModalExcludeTask] = useState(false);
-	const [filteredTasks, setFilteredTasks] = useState([]);
-	const [excludeTask, setExcludeTask] = useState({});
-	const [editOrCreateModalInfo, setEditOrCreateModalInfo] = useState({
-		open: false,
-	});
+  const handleOpenModal = (task) => {
+    setExcludeTask(task);
+    setOpenModalExcludeTask(true);
+  };
 
-	const handleOpenModal = (task) => {
-		setExcludeTask(task);
-		setOpenModalExcludeTask(true);
-	};
+  const handleCloseModal = () => {
+    setOpenModalExcludeTask(false);
+  };
 
-	const handleCloseModal = () => {
-		setOpenModalExcludeTask(false);
-	};
+  const handleCloseEditOrCreate = () => {
+    setEditOrCreateModalInfo({ open: false });
+  };
 
-	const handleCloseEditOrCreate = () => {
-		setEditOrCreateModalInfo({ open: false });
-	};
+  const editOrCreateTask = (method, task) => {
+    const withInitialValue = method === "edit";
 
-	const editOrCreateTask = (method, task) => {
-		const withInitialValue = method === "edit";
+    var fields = [
+      {
+        key: "task",
+        type: "text",
+        label: "Tarefa",
+        rules: [validators.antdRequired()],
 
-		var fields = [
-			{
-				key: "task",
-				type: "text",
-				label: "Tarefa",
-				rules: [validators.antdRequired()],
+        placeholder: "Escreva o nome da tarefa",
+        initialValue: withInitialValue ? task.name : undefined,
+      },
+    ];
 
-				placeholder: "Escreva o nome da tarefa",
-				initialValue: withInitialValue ? task.name : undefined,
-			},
-		];
+    const modalData = {
+      title: "",
+      fields: fields,
 
-		const modalData = {
-			title: "",
-			fields: fields,
+      open: true,
+      cancel: handleCloseEditOrCreate,
+    };
 
-			open: true,
-			cancel: handleCloseEditOrCreate,
-		};
+    if (method === "edit") {
+      modalData.title = "Editar Tarefa";
+      modalData.onSubmit = updateTask(task._id);
+    } else {
+      modalData.title = "Criar Tarefa";
+      modalData.onSubmit = createTask;
+    }
 
-		if (method === "edit") {
-			modalData.title = "Editar Tarefa";
-			modalData.onSubmit = updateTask(task._id);
-		} else {
-			modalData.title = "Criar Tarefa";
-			modalData.onSubmit = createTask;
-		}
+    setEditOrCreateModalInfo(modalData);
+  };
 
-		setEditOrCreateModalInfo(modalData);
-	};
-
-	const createTask = async (task) => {
-		var hide = message.loading("Criando...");
+  const createTask = async (task) => {
+    var hide = message.loading("Criando...");
 
     const { Tarefa } = task;
-		try {
-			const newTask = {
-				name: Tarefa,
-			};
-			await createTaskMutation({ variables: { data: newTask } });
-			hide();
-			message.success("Criada com sucesso", 2.5);
-			refetch();
-		} catch (err) {
-			console.error(err);
-			hide();
-			message.error("Houve um problema, tente novamente", 2.5);
-		}
+    try {
+      const newTask = {
+        name: Tarefa,
+      };
+      await createTaskMutation({ variables: { data: newTask } });
+      hide();
+      message.success("Criada com sucesso", 2.5);
+      refetch();
+    } catch (err) {
+      console.error(err);
+      hide();
+      message.error("Houve um problema, tente novamente", 2.5);
+    }
 
-		handleCloseEditOrCreate();
-	};
+    handleCloseEditOrCreate();
+  };
 
-	const updateTask = (taskId) => async (task) => {
-		var hide = message.loading("Atualizando tarefa...");
+  const updateTask = (taskId) => async (task) => {
+    var hide = message.loading("Atualizando tarefa...");
 
-		const { Tarefa } = task;
+    const { Tarefa } = task;
 
-		try {
-			const newTask = {
-				name: Tarefa,
-			};
+    try {
+      const newTask = {
+        name: Tarefa,
+      };
 
-			await updateTaskMutation({
-				variables: { taskId, data: newTask },
-			});
+      await updateTaskMutation({
+        variables: { taskId, data: newTask },
+      });
 
-			hide();
-			message.success("Atualizada com sucesso", 2.5);
-			refetch();
-		} catch (err) {
-			console.error(err);
-			hide();
-			message.error("Houve um problema, tente novamente", 2.5);
-		}
-		handleCloseEditOrCreate();
-	};
+      hide();
+      message.success("Atualizada com sucesso", 2.5);
+      refetch();
+    } catch (err) {
+      console.error(err);
+      hide();
+      message.error("Houve um problema, tente novamente", 2.5);
+    }
+    handleCloseEditOrCreate();
+  };
 
-	const handleExcludeTask = async (taskId) => {
-		var hide = message.loading("Excluindo tarefa...");
-		try {
-			await deleteTaskMutation({ variables: { taskId } });
-			hide();
-			message.success("Excluída com sucesso", 2.5);
-		} catch (err) {
-			console.error(err);
-			hide();
-			message.error("Houve um problema, tente novamente", 2.5);
-		}
-		refetch();
-		setOpenModalExcludeTask(false);
-	};
+  const handleExcludeTask = async (taskId) => {
+    var hide = message.loading("Excluindo tarefa...");
+    try {
+      await deleteTaskMutation({ variables: { taskId } });
+      hide();
+      message.success("Excluída com sucesso", 2.5);
+    } catch (err) {
+      console.error(err);
+      hide();
+      message.error("Houve um problema, tente novamente", 2.5);
+    }
+    refetch();
+    setOpenModalExcludeTask(false);
+  };
 
-	const handleSearchTask = (e) => {
-		if (e.target.value !== "") {
-			const filteredMembersAfterForEach = allTasksData?.members.filter(
-				({ name }) => diacriticCaseInsensitiveMatch(name, e.target.value)
-			);
+  const handleSearchTask = (e) => {
+    if (e.target.value !== "") {
+      setFilteredTasks(
+        data?.tasks.filter(({ name }) =>
+          diacriticCaseInsensitiveMatch(name, e.target.value)
+        )
+      );
+    } else {
+      setFilteredTasks(data.tasks);
+    }
+  };
 
-			setFilteredTasks(filteredMembersAfterForEach);
-		} else {
-			setFilteredTasks([...allTasksData?.members]);
-		}
-	};
+  useEffect(() => {
+    if (data?.tasks) setFilteredTasks(data.tasks);
+  }, [data?.tasks]);
 
-	useEffect(() => {
-		if (allTasksData) setFilteredTasks([...allTasksData?.members]);
-	}, [allTasksData]);
-
-	if (loading)
-		return (
-			<Skeleton
-				paragraph={{ rows: 4 }}
-				size={"large"}
-				active={tasksLoading}
-				loading={tasksLoading}
-			/>
-		);
-	else if (error) {
-		console.log(tasksError);
-		message.error("Houve um problema, tente recarregar a pagina", 2.5);
-		return <h1>Erro, recarregue a pagina</h1>;
+  if (loading)
+    return (
+      <Skeleton
+        paragraph={{ rows: 4 }}
+        size={"large"}
+        active={loading}
+        loading={loading}
+      />
+    );
+  else if (error) {
+    console.log(error);
+    message.error("Houve um problema, tente recarregar a pagina", 2.5);
+    return <h1>Erro, recarregue a pagina</h1>;
   }
-	// else if (errorRoles) {
-	// 	console.log(errorRoles);
-	// 	message.error("Houve um problema, tente recarregar a pagina", 2.5);
-	// 	return <h1>Erro, recarregue a pagina</h1>;
-	// } else if (errorTribes) {
-	// 	console.log(errorTribes);
-	// 	message.error("Houve um problema, tente recarregar a pagina", 2.5);
-	// 	return <h1>Erro, recarregue a pagina</h1>;
-	// }
 
-	return (
-		<TasksComponent theme={themeColors}>
-			<div className="iconWithTitle">
-				<TeamOutlined className="svgIcon" />
-				<h1>Gestão de Tarefas</h1>
-			</div>
-			<div className="addAndSearchTaskArea">
-				<InputText
-					icon={searchIcon}
-					placeholder="Pesquisar tarefa"
-					onChange={(e) => handleSearchTask(e)}
-				/>
-				<CommonButton
-					buttonLabel="Adicionar nova tarefa"
-					color={themeColors.green}
-					nowrap
-					width="215px"
-					onClick={() => editOrCreateTask("new")}
-				/>
-			</div>
+  return (
+    <TasksComponent theme={themeColors}>
+      <div className="iconWithTitle">
+        <TeamOutlined className="svgIcon" />
+        <h1>Gestão de Tarefas</h1>
+      </div>
+      <div className="addAndSearchTaskArea">
+        <InputText
+          icon={searchIcon}
+          placeholder="Pesquisar tarefa"
+          onChange={(e) => handleSearchTask(e)}
+        />
+        <CommonButton
+          buttonLabel="Adicionar nova tarefa"
+          color={themeColors.green}
+          nowrap
+          width="215px"
+          onClick={() => editOrCreateTask("new")}
+        />
+      </div>
 
-			<Table
-				scroll={{ x: true }}
-				dataSource={filteredTasks}
-				pagination={false}
-			>
-				<Column title="Tarefa" dataIndex="name" key="name" />
-				{/* <Column
-					title="Tribo"
-					dataIndex="tribe"
-					key="tribe"
-					width={200}
-					render={(tribe) =>
-						tribe && (
-							<DefaultLabel labelColor={tribe.color} labelText={tribe.name} />
-						)
-					}
-				/>
-				<Column
-					title="Cargo"
-					dataIndex="role"
-					key="role"
-					width={200}
-					render={(role) => <DefaultLabel labelText={role.name} />}
-				/>
-				<Column
-					title="Assessor"
-					dataIndex="responsible"
-					key="responsible.name"
-					render={(responsible) => responsible?.name}
-				/> */}
-				<Column
-					key="action"
-					width={120}
-					render={(data) => (
-						<ActionsDiv>
-							<Tooltip
-								placement="topLeft"
-								title={"Editar"}
-								onClick={() => editOrCreateTask("edit", data)}
-							>
-								<EditOutlined />
-							</Tooltip>
+      <Table scroll={{ x: true }} dataSource={filteredTasks} pagination={false}>
+        <Column title="Tarefa" dataIndex="name" key="name" />
+        <Column
+          key="action"
+          width={120}
+          render={(data) => (
+            <ActionsDiv>
+              <Tooltip
+                placement="topLeft"
+                title={"Editar"}
+                onClick={() => editOrCreateTask("edit", data)}
+              >
+                <EditOutlined />
+              </Tooltip>
 
-							<Tooltip placement="topLeft" title={"Excluir"}>
-								<RestOutlined onClick={() => handleOpenModal(data)} />
-							</Tooltip>
-						</ActionsDiv>
-					)}
-				/>
-			</Table>
-			<ConfirmationModal
-				title="Apagar tarefa"
-				content={`Deseja mesmo apagar a tarefa "${excludeTask.name}"?`}
-				isVisible={openModalExcludeTask}
-				handleOk={() => handleExcludeTask(excludeTask._id)}
-				handleCancel={handleCloseModal}
-			/>
-			<FormModal {...editOrCreateModalInfo} />
-		</TasksComponent>
-	);
+              <Tooltip placement="topLeft" title={"Excluir"}>
+                <RestOutlined onClick={() => handleOpenModal(data)} />
+              </Tooltip>
+            </ActionsDiv>
+          )}
+        />
+      </Table>
+      <ConfirmationModal
+        title="Apagar tarefa"
+        content={`Deseja mesmo apagar a tarefa "${excludeTask.name}"?`}
+        isVisible={openModalExcludeTask}
+        handleOk={() => handleExcludeTask(excludeTask._id)}
+        handleCancel={handleCloseModal}
+      />
+      <FormModal {...editOrCreateModalInfo} />
+    </TasksComponent>
+  );
 };
 
 export default Tasks;
