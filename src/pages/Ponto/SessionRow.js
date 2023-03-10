ffc1b8b916c4d7c1132cb1daa10797ceedf4a11d
Leandro Lopo <leandro.loopo@gmail.com>
2023-03-10 18:23:48 -0300

    mudança na proporção das divs da home  (modalidade, chegada, tempo)

--- src/pages/Ponto/SessionRow.js
+++ src/pages/Ponto/SessionRow.js
@@ -9,60 +9,60 @@ import { ThemeContext } from "../../context/ThemeProvider";
 import logoutPointIcon from "../../assets/logoutPointIcon.svg";
 
 const SessionRow = ({ session, onLogout, ...props }) => {
-	const { themeColors } = useContext(ThemeContext);
+  const { themeColors } = useContext(ThemeContext);
 
-	const { member } = session;
-	function handleLogout() {
-		onLogout && onLogout(session);
-	}
+  const { member } = session;
+  function handleLogout() {
+    onLogout && onLogout(session);
+  }
 
-	return (
-		<tr {...props} className="d-flex">
-			<td className="col-6 col-sm-5">
-				<LoggedMembers
-					name={member.name}
-					imageLink={member.imageLink}
-					tribe={member?.tribe}
-					role={member?.role?.name}
-					description={member.status}
-					recognition={member.recognition}
-				/>
-			</td>
-			<td className="col-2 d-none d-sm-flex align-items-center justify-content-center">
-				<div className="d-flex">
-					<PresentialDisplayer
-						isPresential={session.isPresential}
-						presentialColor={themeColors.green}
-					/>
-				</div>
-			</td>
-			<td className="col-2 d-none d-sm-flex align-items-center justify-content-center">
-				<div className="d-flex">
-					<HourDisplayer
-						hour={session.start}
-						hourColor={themeColors.green}
-						dateOrTime={"date"}
-					/>
-				</div>
-			</td>
-			<td className="col-3 col-sm-2 d-flex align-items-center justify-content-center">
-				<div className="d-flex">
-					<DurationDisplayer
-						startTime={session.start}
-						color={themeColors.yellow}
-					/>
-				</div>
-			</td>
-			<td className="col-1 d-block d-sm-none"></td>
-			<td className="col-2 col-sm-1 d-flex align-items-center justify-content-center">
-				<Button
-					className="w-100"
-					icon={<img src={logoutPointIcon} alt="Deslogar" />}
-					onClick={handleLogout}
-				/>
-			</td>
-		</tr>
-	);
+  return (
+    <tr {...props} className="d-flex justify-content-between">
+      <td className="col-6 col-sm-5 col-md-6">
+        <LoggedMembers
+          name={member.name}
+          imageLink={member.imageLink}
+          tribe={member?.tribe}
+          role={member?.role?.name}
+          description={member.status}
+          recognition={member.recognition}
+        />
+      </td>
+      <td className="col-2 d-none d-sm-flex align-items-center justify-content-center">
+        <div className="d-flex">
+          <PresentialDisplayer
+            isPresential={session.isPresential}
+            presentialColor={themeColors.green}
+          />
+        </div>
+      </td>
+      <td className="col-1 col-sm-2 col-md-1 d-none d-sm-flex align-items-center justify-content-center">
+        <div className="d-flex">
+          <HourDisplayer
+            hour={session.start}
+            hourColor={themeColors.green}
+            dateOrTime={"date"}
+          />
+        </div>
+      </td>
+      <td className="col-2 col-sm-2 col-md-1 d-flex align-items-center justify-content-center">
+        <div className="d-flex">
+          <DurationDisplayer
+            startTime={session.start}
+            color={themeColors.yellow}
+          />
+        </div>
+      </td>
+      <td className="col-1 d-block d-sm-none"></td>
+      <td className="col-2 col-sm-1 d-flex align-items-center justify-content-center">
+        <Button
+          className="w-100"
+          icon={<img src={logoutPointIcon} alt="Deslogar" />}
+          onClick={handleLogout}
+        />
+      </td>
+    </tr>
+  );
 };
 
 export default SessionRow;
--- src/pages/Ponto/SessionsTable.js
+++ src/pages/Ponto/SessionsTable.js
@@ -3,60 +3,60 @@ import SessionRow from "./SessionRow";
 import ReactRotatingText from "react-rotating-text";
 
 const SessionsTable = ({ sessions, onLogout, ...props }) => {
-	return (
-		// <div class="table-responsive">
-		<table className="table table-borderless" {...props}>
-			<thead>
-				<tr className="customHeader d-flex">
-					<th className="col-6 col-sm-5">
-						<p className="m-0 ms-4">Membro</p>
-					</th>
-					<th className="col-2 text-center d-none d-sm-block">
-						<p className="m-0 text-center">Modalidade</p>
-					</th>
-					<th className="col-2 text-center d-none d-sm-block">
-						<p className="m-0 text-center">Chegada</p>
-					</th>
-					<th className="col-3 col-sm-2 text-center">
-						<p className="m-0 text-center">Tempo</p>
-					</th>
-					<th scope="col-1 d-block d-sm-none"></th>
-					<th scope="col-2 col-sm-1"></th>
-				</tr>
-			</thead>
-			<tbody>
-				{sessions?.map((session) => (
-					<SessionRow
-						key={session.member._id}
-						session={session}
-						onLogout={onLogout}
-					/>
-				))}
+  return (
+    // <div class="table-responsive">
+    <table className="table table-borderless" {...props}>
+      <thead>
+        <tr className="customHeader d-flex justify-content-between">
+          <th className="col-6 col-sm-5 col-md-6">
+            <p className="m-0 ms-4">Membro</p>
+          </th>
+          <th className="col-2 text-center d-none d-sm-block">
+            <p className="m-0 text-center">Modalidade</p>
+          </th>
+          <th className="col-1 col-sm-2 col-md-1 text-center d-none d-sm-block">
+            <p className="m-0 text-center">Chegada</p>
+          </th>
+          <th className="col-2 col-sm-2 col-md-1 text-center">
+            <p className="m-0 text-center">Tempo</p>
+          </th>
+          <th className="col-1 d-block d-sm-none"></th>
+          <th className="col-2 col-sm-1"></th>
+        </tr>
+      </thead>
+      <tbody>
+        {sessions?.map((session) => (
+          <SessionRow
+            key={session.member._id}
+            session={session}
+            onLogout={onLogout}
+          />
+        ))}
 
-				{(!sessions || sessions.length === 0) && (
-					<tr>
-						<td colSpan="4">
-							<h1 style={{ color: "#fff", fontSize: "30px" }}>
-								Trabalhe enquanto eles{" "}
-								<ReactRotatingText
-									typingInterval={120}
-									deletingInterval={80}
-									items={[
-										"dormem...",
-										"comem (???)",
-										"estudam rsrs",
-										"dão migué B)",
-										"... isso não faz mais sentido",
-									]}
-								/>
-							</h1>
-						</td>
-					</tr>
-				)}
-			</tbody>
-		</table>
-		// </div>
-	);
+        {(!sessions || sessions.length === 0) && (
+          <tr>
+            <td colSpan="4">
+              <h1 style={{ color: "#fff", fontSize: "30px" }}>
+                Trabalhe enquanto eles{" "}
+                <ReactRotatingText
+                  typingInterval={120}
+                  deletingInterval={80}
+                  items={[
+                    "dormem...",
+                    "comem (???)",
+                    "estudam rsrs",
+                    "dão migué B)",
+                    "... isso não faz mais sentido",
+                  ]}
+                />
+              </h1>
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+    // </div>
+  );
 };
 
 export default SessionsTable;
