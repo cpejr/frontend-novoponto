a6e2c23089c60c50fb921f0d7e4c23e36ad26733
Leandro Lopo <leandro.loopo@gmail.com>
2023-03-24 20:05:40 -0300

    adição da página de reconhecimentos
    página de reconhecimentos com rota pronta

---
+++ src/pages/Administration/Badges/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Badges = () => {
+  return <h1>Hello World</h1>;
+};
+
+export default Badges;
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,76 +5,84 @@ import SidebarMenu from "../components/organisms/Menu";
 import RestrictedRoute from "./RestrictedRoute";
 
 import {
-	Profile,
-	HourChanges,
-	HourConsultation,
-	Ponto,
-	StandBy,
-	UpdateNews,
-	MandatoryHours,
-	Members,
-	Roles,
-	HourFollowing,
-	Login,
+  Profile,
+  HourChanges,
+  HourConsultation,
+  Ponto,
+  StandBy,
+  UpdateNews,
+  MandatoryHours,
+  Members,
+  Tribes,
+  Badges,
+  Roles,
+  HourFollowing,
+  Login,
 } from "../pages";
-import Tribes from "../pages/Administration/Tribes/index.js";
+//import Tribes from "../pages/Administration/Tribes/index.js";
+//import Badges from "../pages/Administration/Badges/index.js";
 
 function Menu() {
-	return(
-		<SidebarMenu>
-			<Switch>
-				<Route path="/ponto" exact component={Ponto} />
-				<Route path="/ponto/alteracaodehoras" component={HourChanges} />
-				<Route path="/ponto/consultadehoras" component={HourConsultation} />
-				<Route path="/ponto/profile" component={Profile} />
-				<Route path="/ponto/standby" component={StandBy} />
+  return (
+    <SidebarMenu>
+      <Switch>
+        <Route path="/ponto" exact component={Ponto} />
+        <Route path="/ponto/alteracaodehoras" component={HourChanges} />
+        <Route path="/ponto/consultadehoras" component={HourConsultation} />
+        <Route path="/ponto/profile" component={Profile} />
+        <Route path="/ponto/standby" component={StandBy} />
 
-				<RestrictedRoute
-					minAccessLevel={1}
-					path="/ponto/membros"
-					component={Members}
-				/>
-				<RestrictedRoute
-					minAccessLevel={1}
-					path="/ponto/cargos"
-					component={Roles}
-				/>
-				<RestrictedRoute
-					minAccessLevel={1}
-					path="/ponto/tribos"
-					component={Tribes}
-				/>
-				<RestrictedRoute
-					minAccessLevel={1}
-					path="/ponto/acompanhamento"
-					component={HourFollowing}
-				/>
-				<RestrictedRoute
-					path="/ponto/atualizarnoticias"
-					minAccessLevel={1}
-					component={UpdateNews}
-				/>
-				{/* <RestrictedRoute
+        <RestrictedRoute
+          minAccessLevel={1}
+          path="/ponto/membros"
+          component={Members}
+        />
+        <RestrictedRoute
+          minAccessLevel={1}
+          path="/ponto/cargos"
+          component={Roles}
+        />
+        <RestrictedRoute
+          minAccessLevel={1}
+          path="/ponto/tribos"
+          component={Tribes}
+        />
+        <RestrictedRoute
+          minAccessLevel={1}
+          path="/ponto/reconhecimentos"
+          component={Badges}
+        />
+        <RestrictedRoute
+          minAccessLevel={1}
+          path="/ponto/acompanhamento"
+          component={HourFollowing}
+        />
+        <RestrictedRoute
+          path="/ponto/atualizarnoticias"
+          minAccessLevel={1}
+          component={UpdateNews}
+        />
+
+        {/* <RestrictedRoute
 					path="/ponto/horarioobrigatorio"
 					minAccessLevel={1}
 					component={MandatoryHours}
 				/> */}
-
-			</Switch>
-		</SidebarMenu>
-	)
+      </Switch>
+    </SidebarMenu>
+  );
 }
 
 const Routes = () => {
-	return (
-		<BrowserRouter>
-			<Route exact path="/">
-				{<Redirect to="/ponto"/>}
-			</Route>
-			<Route path="/login" component={Login} />
-			<Route path="/ponto" component={Menu} />
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Route exact path="/">
+        {<Redirect to="/ponto" />}
+      </Route>
+      <Route path="/login" component={Login} />
+      <Route path="/ponto" component={Menu} />
+    </BrowserRouter>
+  );
 };
 
 export default Routes;
