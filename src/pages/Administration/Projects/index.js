ab1531b0c484ffa11e2de5ec46ea97ad87d81276
SamuelFVG <samuelfelipeif@gmail.com>
2023-08-09 11:23:25 -0300

    feat: modais edit e create

--- src/pages/Administration/Projects/index.js
+++ src/pages/Administration/Projects/index.js
@@ -5,40 +5,68 @@ import { CommonButton } from "../../../components/atoms";
 import { ThemeContext } from "../../../context/ThemeProvider";
 import { Skeleton, message } from "antd";
 import ProjectRow from "./ProjectRow";
+import FormModal from "../../../components/organisms/FormModal";
+import validators from "../../../services/validators";
 
 const Projects = () => {
-  const { themeColors } = useContext(ThemeContext);
   // não sei se vão continuar como usestates no futuro, tem que ver como que a linkagem será feita
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  if (loading)
-    return (
-      <Skeleton
-        paragraph={{ rows: 4 }}
-        size={"large"}
-        active={loading}
-        loading={loading}
-      />
-    );
+  const { themeColors } = useContext(ThemeContext);
+  const [editOrCreateModalInfo, setEditOrCreateModalInfo] = useState({
+    open: false,
+  });
 
-  if (error) {
-    message.error("Houve um problema, tente recarregar a pagina", 2.5);
-    return <h1>Erro, recarregue a pagina</h1>;
-  }
-
-  const createProject = () => {
-    console.log("Aqui vai o modal de criar o projeto");
+  const handleCloseEditOrCreate = () => {
+    setEditOrCreateModalInfo({ open: false });
   };
-  const editProject = (project) => {
-    console.log("Aqui vai o modal de editar o projeto", project);
+
+  const editOrCreateProject = (method, badge) => {
+    const withInitialValue = method === "edit";
+
+    var fields = [
+      {
+        key: "name",
+        type: "text",
+        label: "Projeto",
+        placeholder: "Digite o nome do projeto",
+        rules: [validators.antdRequired()],
+        initialValue: withInitialValue ? badge.name : undefined,
+      },
+      {
+        key: "area",
+        type: "text",
+        label: "Área",
+        placeholder: "Digite a área do projeto",
+        rules: [validators.antdRequired()],
+        initialValue: withInitialValue ? badge.area : undefined,
+      },
+    ];
+
+    const modalData = {
+      title: "",
+      fields: fields,
+      open: true,
+      cancel: handleCloseEditOrCreate,
+    };
+
+    if (method === "edit") {
+      modalData.title = "Editar Projeto";
+      //modalData.onSubmit = alert("Editar");
+    } else {
+      modalData.title = "Criar Projeto";
+      //modalData.onSubmit = alert("Criar");
+    }
+
+    setEditOrCreateModalInfo(modalData);
   };
   const deleteProject = (project) => {
     console.log("Aqui vai o modal de deletar o projeto", project);
   };
 
+  //um array de objetos será enviado do backend
   const projects = [
-    //um array de objetos será enviado do backend
     {
       _id: 1,
       name: "Doti",
@@ -55,6 +83,21 @@ const Projects = () => {
       area: "Aplicativos",
     },
   ];
+
+  if (loading)
+    return (
+      <Skeleton
+        paragraph={{ rows: 4 }}
+        size={"large"}
+        active={loading}
+        loading={loading}
+      />
+    );
+
+  if (error) {
+    message.error("Houve um problema, tente recarregar a pagina", 2.5);
+    return <h1>Erro, recarregue a pagina</h1>;
+  }
   return (
     <ProjectsComponent>
       <div className="iconWithTitle">
@@ -66,7 +109,7 @@ const Projects = () => {
           buttonLabel="Novo projeto"
           color={themeColors.green}
           width="223px"
-          onClick={() => createProject()}
+          onClick={() => editOrCreateProject("create")}
         />
       </div>
       <table className="projectTable">
@@ -84,7 +127,7 @@ const Projects = () => {
               <ProjectRow
                 key={project._id}
                 project={project}
-                onEdit={() => editProject(project)}
+                onEdit={() => editOrCreateProject("edit", project)}
                 onDelete={() => deleteProject(project)}
               />
             ))
@@ -95,6 +138,7 @@ const Projects = () => {
           )}
         </tbody>
       </table>
+      <FormModal {...editOrCreateModalInfo} />
     </ProjectsComponent>
   );
 };
