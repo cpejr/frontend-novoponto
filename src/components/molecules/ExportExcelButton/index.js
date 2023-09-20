76e2632a02c3fcc6f9d3b499e1508dc7ab7fd016
SamuelFVG <samuelfelipeif@gmail.com>
2023-09-20 14:03:37 -0300

    fix: correção na criação de arquivo para evitar crash quando o membro não possui algum campo específico

--- src/components/molecules/ExportExcelButton/index.js
+++ src/components/molecules/ExportExcelButton/index.js
@@ -5,19 +5,18 @@ import CommonButton from "../../atoms/CommonButton";
 import { SessionContext } from "../../../context/SessionProvider";
 
 const ExportExcel = ({ jsonData, archiveName }) => {
+  const { data } = useContext(SessionContext);
+  const { member } = data || {};
 
-  const {data} = useContext(SessionContext);
-  const {member} = data || {};
-  
   const archiveType =
     "application/vnd.openxmlformats-officedocument.sheet;charset=UTF-8";
   const archiveExtension = ".xlsx";
- 
+
   const jsonFormatedData = jsonData.map((session) => {
     return {
       Início: session?.start,
       Fim: session?.end,
-      Duração: session?.formatedDuration,  
+      Duração: session?.formatedDuration,
       "Nome da Tarefa": session?.task.name,
       "É presencial?": session?.isPresential,
     };
@@ -28,17 +27,15 @@ const ExportExcel = ({ jsonData, archiveName }) => {
     const sheet = { Sheets: { data: sheetData }, SheetNames: ["data"] };
     const buffer = XLSX.write(sheet, { bookType: "xlsx", type: "array" });
     const archive = new Blob([buffer], { type: archiveType });
-    FileSaver.saveAs(archive, archiveName + "_" + member.name + "_" + member.role.name + "_" + member.tribe.name + archiveExtension);
+    FileSaver.saveAs(
+      archive,
+      `${archiveName} - ${member?.name} - ${member?.role?.name} - ${member?.tribe?.name}.${archiveExtension}`
+    );
   }
 
   return (
-    <CommonButton
-      onClick={() => exportToExcel()}
-      color="#22762b"
-      nowrap
-    >
+    <CommonButton onClick={() => exportToExcel()} color="#22762b" nowrap>
       Visualizar em Planilha
-
     </CommonButton>
   );
 };
