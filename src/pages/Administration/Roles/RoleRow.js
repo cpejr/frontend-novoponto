d8c25e1eeb3aba45eff850dc55d9917e4892ff47
Adrianus Vieira <adrianusvieira@cpejr.com.br>
2023-10-08 19:04:28 -0300

    fix: fazendo uma validação para ter deapartament ou nao

--- src/pages/Administration/Roles/RoleRow.js
+++ src/pages/Administration/Roles/RoleRow.js
@@ -7,49 +7,58 @@ import { useQuery } from "@apollo/client";
 import { GET_DEPARTAMENT_BY_ID } from "../../../graphql/Departaments";
 
 const RoleRow = ({ role, onEdit, onDelete, ...props }) => {
-	const { availableRoles } = useContext(GlobalsContext);
-	const { loading, data } = useQuery(GET_DEPARTAMENT_BY_ID	, {
-    variables: { 
-			departamentId: role.departamentId
-		},
-  });
-	return (
-		<>
-		{!loading && 
-			<tr {...props}>
-			<td className="roleColumn">{role.name}</td>
-			<td className="isAdmColumn">
-				{role.access > 0 && (
-					<DefaultLabel
-						labelText={
-							availableRoles.find(
-								(availableRole) => availableRole.value === role.access
-							)?.label
-						}
-						labelColor="#FFD100"
-					/>
-				)}
-			</td>
-			<td className="roleColumn">
-			<DefaultLabel
-						labelText={data.departamentById.name}
-						labelColor={data.departamentById.color}
-					/>
-					</td>
-			<td className="editColumn">
-				<Tooltip placement="topLeft" title={"Editar"} onClick={() => onEdit('edit', role, data)}>
-					<EditOutlined />
-				</Tooltip>
-			</td>
-			<td className="garbageColumn">
-				<Tooltip placement="topLeft" title={"Excluir"}>
-					<RestOutlined onClick={onDelete} />
-				</Tooltip>
-			</td>
-		</tr>
-		}
-		</>
-	);
+  const { availableRoles } = useContext(GlobalsContext);
+  if (role.departamentId) {
+    const { loading, data } = useQuery(GET_DEPARTAMENT_BY_ID, {
+      variables: {
+        departamentId: role.departamentId,
+      },
+    });
+  } else {
+    const data = "Nenhum departamento";
+  }
+
+  return (
+    <>
+      {!loading && (
+        <tr {...props}>
+          <td className="roleColumn">{role.name}</td>
+          <td className="isAdmColumn">
+            {role.access > 0 && (
+              <DefaultLabel
+                labelText={
+                  availableRoles.find(
+                    (availableRole) => availableRole.value === role.access
+                  )?.label
+                }
+                labelColor="#FFD100"
+              />
+            )}
+          </td>
+          <td className="roleColumn">
+            <DefaultLabel
+              labelText={data.departamentById.name}
+              labelColor={data.departamentById.color}
+            />
+          </td>
+          <td className="editColumn">
+            <Tooltip
+              placement="topLeft"
+              title={"Editar"}
+              onClick={() => onEdit("edit", role, data)}
+            >
+              <EditOutlined />
+            </Tooltip>
+          </td>
+          <td className="garbageColumn">
+            <Tooltip placement="topLeft" title={"Excluir"}>
+              <RestOutlined onClick={onDelete} />
+            </Tooltip>
+          </td>
+        </tr>
+      )}
+    </>
+  );
 };
 
 export default RoleRow;
