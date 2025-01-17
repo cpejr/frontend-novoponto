467420d385c996aee4759aa7f839d377af14402b
ZeL-ucas <zelucaspomps15@gmail.com>
2025-01-17 16:25:19 -0300

    fix: bugfilter

--- src/components/molecules/SelectBox/index.js
+++ src/components/molecules/SelectBox/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import { CommonSelectBox } from "../../atoms";
 
 const SelectBox = ({ options = [], placeholder, ...props }) => {
+
   const optionsList = options.map((item) => ({
     value: item._id,
     label: item.name,
   }));
-
+  optionsList.push({value:"",label:""})
   return (
     <CommonSelectBox
       optionsList={optionsList}
