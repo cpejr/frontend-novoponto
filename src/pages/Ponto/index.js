af53be06640005661f3495949867b347ceeb09d9
ZeL-ucas <zelucaspomps15@gmail.com>
2025-03-17 11:10:55 -0300

    start of rulexbox

--- src/pages/Ponto/index.js
+++ src/pages/Ponto/index.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 
-import { PontoComponent } from "./styles";
+import { PontoComponent,RulesBox,RulesTitle,RulesFlex,RuleItem } from "./styles";
 import { ThemeContext } from "../../context/ThemeProvider";
 import Sessions from "./Sessions";
 import NewsCarousel from "../../components/molecules/NewsCarousel";
@@ -8,11 +8,27 @@ import { NotificationHome } from "./NotificationHome";
 
 const Ponto = () => {
   const { themeColors } = useContext(ThemeContext);
-
+  const rules = [
+    "Aplauda se alguém tiver indo bem, dê um toque se alguém tiver fazendo algo errado",
+    "Chegar no horário é atraso! 5 minutos de tolerância!",
+    "Sujou, Lavou! Desorganizou, Organizou!",
+    "Sua presença é importante! 3 horas obrigatórias semanais na salinha!",
+    "Câmera desligada é FALTA! Vocês são muito bonitos para ficar de câmera desligada!",
+    "Da salinha pra dentro, pensar duas vezes! Respeite seu local de trabalho e o dos seus colegas!",
+    "Sempre que se sentir incomodado, utilize o canal de ouvidoria! Queremos te ouvir!"
+  ];
   return (
     <PontoComponent theme={themeColors} className="m-0 mt-lg-3 m-lg-5">
       <NotificationHome />
       <NewsCarousel />
+      <RulesBox>
+      <RulesTitle>regras da gestao</RulesTitle>
+      <RulesFlex>
+          {rules.map((regra, index) => (
+            <RuleItem key={index}>{regra}</RuleItem>
+          ))}
+        </RulesFlex>
+      </RulesBox>
       <Sessions />
     </PontoComponent>
   );
--- src/pages/Ponto/styles.js
+++ src/pages/Ponto/styles.js
@@ -33,4 +33,45 @@ const newsComponent = styled.div`
 const TooltipTitle = styled.div`
   text-align: center;
 `;
-export { PontoComponent, newsComponent, TooltipTitle };
+
+const RulesBox = styled.div`
+width: 100%;
+height: 300px;
+border: 2px solid #FFD100; 
+border-radius: 8px;     
+box-sizing: border-box;
+`
+const RulesTitle = styled.div`
+  width: 100%;
+  height: 40px;
+  text-align: center;
+  font-size: 32px;
+  font-weight: bold;
+  color: #FFD100;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-bottom: 3px solid #FFD100;
+`;
+const RulesFlex = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 16px;
+  margin-top: 20px;
+  width: 100%;
+`;
+
+const RuleItem = styled.div`
+  flex: 1 1 calc(14.28% - 16px); /* Aproximadamente 7 colunas */
+  max-width: calc(14.28% - 16px);
+  border: 2px solid yellow;
+  border-radius: 8px;
+  padding: 12px;
+  background-color: #fff;
+  color: #000;
+  font-size: 16px;
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.1);
+  text-align: center;
+  word-break: break-word;
+`;
+export { PontoComponent, newsComponent, TooltipTitle, RulesBox, RulesTitle, RulesFlex,RuleItem };
